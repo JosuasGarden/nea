ada8f0717d55b8b4332c1d030914339827a63fd4
Michael van Straten <michael@vanstraten.de>
2023-05-12 09:12:02 +0200

    added hot reload of i18n using https://kazupon.github.io/vue-i18n/guide/hot-reload.html#advanced-example

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,30 @@ import Utils from './utils';
 
 // Internationalisation.
 Vue.use(VueI18n);
-const i18n = new VueI18n();
+// Load all locales and remember context
+function loadMessages() {
+  const context = require.context('../../i18n/', true, /[a-z0-9-_]+\.json$/i);
+
+  const messages = context
+    .keys()
+    .map((key) => ({ key, locale: key.match(/[a-z0-9-_]+/i)[0] }))
+    .reduce(
+      (messagesx, { key, locale }) => ({
+        ...messagesx,
+        [locale]: context(key),
+      }),
+      {},
+    );
+
+  return { context, messages };
+}
+
+const { context, messages } = loadMessages();
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages,
+});
 
 Vue.use(Buefy, {});
 Vue.config.productionTip = false;
@@ -77,3 +100,17 @@ const v = new Vue({
 });
 
 initConfig(v);
+
+// Hot updates
+if (module.hot) {
+  module.hot.accept(context.id, () => {
+    const { messages: newMessages } = loadMessages();
+
+    Object.keys(newMessages)
+      .filter((locale) => messages[locale] !== newMessages[locale])
+      .forEach((locale) => {
+        messages[locale] = newMessages[locale];
+        i18n.setLocaleMessage(locale, messages[locale]);
+      });
+  });
+}
