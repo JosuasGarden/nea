5407bd44912e474ec8a6e34e3e6722f44a06fa2f
Michael van Straten <michael@vanstraten.de>
2023-05-23 10:01:37 +0200

    Renamed function in ProductProvider interface

--- internal/product-manager/provider/provider.go
+++ internal/product-manager/provider/provider.go
@@ -1,9 +1,11 @@
 package provider
 
 import (
-    "html/template"
+	"html/template"
+
+	"github.com/knadh/listmonk/models"
 )
 
 type ProductProvider interface {
-    RegisterFunctions(*template.FuncMap)
+    ProvideTemplateFunctions(c *models.Campaign) template.FuncMap
 }
